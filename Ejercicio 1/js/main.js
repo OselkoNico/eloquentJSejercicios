d65d6f396cc6f0b4ad1101027b696166eb2f3002
Osel_Nicolás <oselko123@gmail.com>
2026-02-18 15:36:30 +0100

    Ejercicio 1

---
+++ Ejercicio 1/js/main.js
@@ -0,0 +1,17 @@
+for (let numero = 1; numero <= 7; numero++) {
+    if (numero === 1) {
+        console.log("#");
+    }else if (numero === 2) {
+        console.log("##");
+    }else if (numero === 3) {
+        console.log("###");
+    }else if (numero === 4) {
+        console.log("####");
+    }else if (numero === 5) {
+        console.log("#####");
+    }else if (numero === 6) {
+        console.log("######");
+    }else if (numero === 7) {
+        console.log("#######");
+    }
+}
