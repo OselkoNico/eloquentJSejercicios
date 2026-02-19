785d3ec1e16e98ddb795a5a24286815c14b8e4cc
Osel_Nicolás <oselko123@gmail.com>
2026-02-19 13:33:48 +0100

    add todo lo restante

---
+++ Ejercicio3.4/js/ejercicio3.4.js
@@ -0,0 +1,17 @@
+let size=6;
+let board="";
+for(let x=0;x<size;x++){
+    for(let y=0;y<size;y++){
+        if((x===0)||(x===size-1)||(y===0)||(y===size-1))
+            board+="X";
+    else if((x===1)||(x===size-2)||(y===1)||(y===size-2))
+        board+="O";
+else if((x+y)%2==0){
+board+=" ";
+} else{
+    board+="X";
+} 
+}
+board+="\n";
+}
+console.log(board);
