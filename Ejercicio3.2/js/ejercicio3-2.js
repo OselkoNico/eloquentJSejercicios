7824a105f0201a38d5d947c3274b83190d9a7d8a
Osel_Nicolás <oselko123@gmail.com>
2026-02-19 12:12:24 +0100

    Ejercicio IA Completado

---
+++ Ejercicio3.2/js/ejercicio3-2.js
@@ -0,0 +1,13 @@
+let size = 6;
+let board ="";
+for (let x=0; x<size; x++){
+    for(let y=0; y<size; y++){
+           if (((x=== 0)||(x === size-1))||((y===0)||(y === size-1))) 
+            board+="X";
+            else {
+            board+=".";
+           }
+        }
+    board+="\n";
+}
+console.log(board);
