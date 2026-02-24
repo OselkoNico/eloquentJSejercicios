31aa2d568c3be6bdeb1a26e0769e02e8a74385bc
Osel_Nicolás <oselko123@gmail.com>
2026-02-24 16:04:00 +0100

    Ejercicio 9 y solucion

--- Capítulo 4/Ejercicio 9/js/Ejercicio9.js
+++ Capítulo 4/Ejercicio 9/js/Ejercicio9.js
@@ -1,10 +1,30 @@
 function arrayToList(array){
-    let list={value: "", rest: list}
-
+    let list=null;
+    for (i=array.length-1;i>=0;i--){
+       list={value: array[i], rest: list}}
+       return list; 
     }
 
+function listToArray(list){
+    let resultado=[];
+    while(list !== null){
+        resultado.push(list.value);
+        list=list.rest;
+    }
+    return resultado;
+}
+function prepend(value,rest){
+    return {value: value, rest: rest};
+}
 
+function nth(list,n){
+    if (list === null)
+        return undefined;
+    if (n===0)
+        return list.value;
+        return nth(list.rest, n-1);
 
+}
 
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
